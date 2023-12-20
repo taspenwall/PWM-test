3a9dcd78fad3049298a061bdd25f178d84ce6595
Tom Aspenwall <Tom@linuxtom.com>
2023-12-20 10:37:09 -0700

    Code works for longer period but not fast enough

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project to use the Milkv duo pwm to drive a ws2812 full color led
--- pwm-test.c
+++ pwm-test.c
@@ -22,15 +22,22 @@
 #define PWM_OE 0x0D0
 #define base PWM_BASE+PWM_GROUP
 
+#define CLK_BASE 0x03002000
+#define CLK_PWM 0x120
+
 
 //settings for pwm
-#define freq  900000// 800kHz
-#define Hduty 75 //75%
-#define Lduty 25 //25%
+#define freq  125// 125 is the calcualted for 800kHz calulated frequency,  It seem that I have to go way lower for a 1.25us period.
+#define Hduty  12//56% is the claculated duty cycle for .7us high time to write a 1
+#define Lduty  1//28%  is the claculated duty cycle for .35us high time   to write a 0
 #define count 1//30 pulses
 #define start 0b0100
-#define count_mode 1<<10
+#define pwm_mode ((1<<10) | (1<<2)) // set the pmode to to pulse 1<<num+8 and set the polarity to control the high period 1<<2
 #define pwmoe 0b0100
+
+
+
+
 
 
 void setup_pwm(){
@@ -38,26 +45,28 @@ void setup_pwm(){
     //set the pwm registers
     devmap_writel(PWM_OE, pwmoe); //output enable
     
-    devmap_writel(PWM_MODE, count_mode); //set count mode
+    devmap_writel(PWM_MODE, pwm_mode); //set count mode and polarity
     devmap_writel(PWM_START, 0); //set start to 0
     devmap_writel(PERIOD2, freq); //set period to 800kHz
     devmap_writel(PCOUNT2, count);  //set for 1 pulse
+    
+
 }
 
 void kick_and_wait(){
     devmap_writel(PWM_START, start);
-    for(volatile int i = 0; i < 2; i++); //wait for pulse to finish
+    while(devmap_readl(PWM_DONE) == 0);
 
 }
 void run_pulse (int value){
     devmap_writel(PWM_START, 0); //clear the start bit
     if(value == 1){
-        devmap_writel(HLPERIDO2, (Hduty*freq)/100); // set helper period to high duty
+       devmap_writel(HLPERIDO2, Hduty*freq/100); // set helper period to high duty
         kick_and_wait();
         }
     
     else{
-        devmap_writel (HLPERIDO2, (Lduty*freq)/100); //set helper period to low duty
+        devmap_writel (HLPERIDO2, Lduty*freq/100); //set helper period to low duty 23 works
         kick_and_wait();
     }
 }
@@ -79,22 +88,27 @@ void printbin(uint32_t x) {
 int main() {
 
     set_func(2, 0x07);
-    printf("Ready set go\n");
+    printf("Ready set go 1,0,0\n");
 
-
+   
 
     //set the pwm registers
     setup_pwm();
-    run_pulse(1);
-    run_pulse(0);
-    run_pulse(1);
-    run_pulse(0);
+    
     run_pulse(1);
     run_pulse(1);
     run_pulse(0);
     run_pulse(0);
-    cleanup();
+    
 
+    
+
+    printf("mode: ");
+
+
+    
+    cleanup();
+    
     
 
     return 0;
